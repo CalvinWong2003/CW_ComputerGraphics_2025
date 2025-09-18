f440c1d9568045913595299c5a3b9b22d7055f86
Student Calvin Wong (TL_KCOMP_B_Y3) <t00247209@mymtu.ie>
2025-09-18 09:24:40 +0100

    Signed-off-by: Student Calvin Wong (TL_KCOMP_B_Y3) <t00247209@mymtu.ie>

---
+++ CG_Pipeline_Initials/Assets/Pipeline_Initials.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipeline_Initials
+{
+    List<Vector3> vertices;
+    List<Vector3Int> faces;
+
+    public Pipeline_Initials()
+    {
+        defineVertices();
+        defineFaces();
+        CreateUnityGameObject();
+    }
+    
+    void defineVertices()
+    {
+        vertices = new List<Vector3>();
+        
+        //Front face
+        vertices.Add(new Vector3( -4f, 8f, 2f )); // 0
+        vertices.Add(new Vector3( -4f, -8f, 2f )); // 1
+        vertices.Add(new Vector3( 1f, -8f, 2f )); // 2
+        vertices.Add(new Vector3( 4f, -5f, 2f )); // 3
+        vertices.Add(new Vector3( -2f, -5f, 2f )); // 4
+        vertices.Add(new Vector3( -2f, 5f, 2f )); // 5
+        vertices.Add(new Vector3( 4f, 5f, 2f )); // 6
+        vertices.Add(new Vector3( 1f, 8f, 2f )); // 7
+        
+        //Back Face
+        vertices.Add(new Vector3( -4f, 8f, -2f )); // 8
+        vertices.Add(new Vector3( -4f, -8f, -2f )); // 9
+        vertices.Add(new Vector3( 1f, -8f, -2f )); // 10
+        vertices.Add(new Vector3( 4f, -5f, -2f )); // 11
+        vertices.Add(new Vector3( -2f, -5f, -2f )); // 12
+        vertices.Add(new Vector3( -2f, 5f, -2f )); // 13
+        vertices.Add(new Vector3( 4f, 5f, -2f )); // 14
+        vertices.Add(new Vector3( 1f, 8f, -2f )); // 15
+    }
+
+    void defineFaces()
+    {
+        faces = new List<Vector3Int>();
+        
+        //Front face
+        faces.Add(new Vector3Int(0, 6, 7));
+        faces.Add(new Vector3Int(0, 5, 6));
+        faces.Add(new Vector3Int(0, 1, 5));
+        faces.Add(new Vector3Int(1, 4, 5));
+        faces.Add(new Vector3Int(1, 2, 3));
+        faces.Add(new Vector3Int(1, 3, 4));
+        
+        //Back Face
+        faces.Add(new Vector3Int(8, 14, 15));
+        faces.Add(new Vector3Int(8, 13, 14));
+        faces.Add(new Vector3Int(8, 9, 13));
+        faces.Add(new Vector3Int(9, 12, 13));
+        faces.Add(new Vector3Int(9, 10, 11));
+        faces.Add(new Vector3Int(9, 11, 12));
+    }
+    
+    public GameObject CreateUnityGameObject()
+    {
+        Mesh mesh = new Mesh();
+        GameObject newGO = new GameObject();
+     
+        MeshFilter mesh_filter = newGO.AddComponent<MeshFilter>();
+        MeshRenderer mesh_renderer = newGO.AddComponent<MeshRenderer>();
+
+        List<Vector3> coords = new List<Vector3>();
+        List<int> dummy_indices = new List<int>();
+        /*List<Vector2> text_coords = new List<Vector2>();
+        List<Vector3> normalz = new List<Vector3>();*/
+
+        for (int i = 0; i < faces.Count; i++)
+        {
+            //Vector3 normal_for_face = normals[i];
+
+            //normal_for_face = new Vector3(normal_for_face.x, normal_for_face.y, -normal_for_face.z);
+
+            coords.Add(vertices[faces[i].x]); dummy_indices.Add(i * 3); //text_coords.Add(texture_coordinates[texture_index_list[i].x]); normalz.Add(normal_for_face);
+
+            coords.Add(vertices[faces[i].y]); dummy_indices.Add(i * 3 + 2); //text_coords.Add(texture_coordinates[texture_index_list[i].y]); normalz.Add(normal_for_face);
+
+            coords.Add(vertices[faces[i].z]); dummy_indices.Add(i * 3 + 1); //text_coords.Add(texture_coordinates[texture_index_list[i].z]); normalz.Add(normal_for_face);
+        }
+
+        mesh.vertices = coords.ToArray();
+        mesh.triangles = dummy_indices.ToArray();
+        /*mesh.uv = text_coords.ToArray();
+        mesh.normals = normalz.ToArray();*/
+        mesh_filter.mesh = mesh;
+
+        return newGO;
+    }
+}
